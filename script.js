cdfef904fd33be2d4bd19f343f9ff8306166d2b7
Grg-Sid <siddhantgurung007@gmail.com>
2022-10-11 09:06:49 +0530

    console output error

--- script.js
+++ script.js
@@ -1,27 +1,67 @@
-function validate() {
-    const form = document.getElementById('form').value;
-    const username = document.getElementById('name').value;
-    //alert(username);
-    const number = document.getElementById('phnmb').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const cpassword = document.getElementById('cnfmpass').value;
+const form = document.getElementById('form');
+const username = document.getElementById('name');
+const phone = document.getElementById('phnmb');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cnfmpass');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validate();
+})
+
+const validate = () => {
+    //    const form = document.getElementById('form').value;
+    const usernameVal = username.value.trim();
+    const phoneVal = phone.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
 
 
     // REGEX //
     var userCheck = /^[A-Za-z. ]{3,25}$/;
     var passwordCheck = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,18}$/;
-    var emaiCheck = /^[a-zA-Z0-9_.]{3,}[@][a-zA-Z]{3,}[.]{1}[A-Za-z.]{2,8}$/;
+    var emailCheck = /^[a-zA-Z0-9_.]{3,}[@][a-zA-Z]{3,}[.]{1}[A-Za-z.]{2,8}$/;
     var phoneCheck = /^[0-9]{10}$/;
 
-    // if (userCheck.test(username)) {
-    //     setSuccesMsg(username);
-    // }
-    // else {
-    //     setErrorMsg(username, "Invalid username");
-    //     return false;
-    // }
+    // username //
+    if (userCheck.test(usernameVal)) {
+        setSuccesMsg(username);
+    }
+    else {
+        setErrorMsg(username, "Invalid Username");
+    }
 
+    // number //
+    if (phoneCheck.test(phoneVal)) {
+        setSuccesMsg(phone);
+    }
+    else {
+        setErrorMsg(phone, "Invalid Phonenumber");
+    }
+    // email //
+    if (emailCheck.test(emailVal)) {
+        setSuccesMsg(email);
+    }
+    else {
+        setErrorMsg(email, "Invalid Email");
+    }
+    // password //
+    if (passwordCheck.test(passwordVal)) {
+
+        if (passwordVal === cpasswordVal) {
+            setSuccesMsg(cpassword);
+            setSuccesMsg(password);
+        }
+        else {
+            setErrorMsg(cpassword, "Doesn't Match");
+            setErrorMsg(password, "Doesn't Match");
+        }
+    }
+    else {
+        setErrorMsg(password, "Invalid Password");
+    }
 }
 //function  
 function setErrorMsg(input, errormsgs) {
@@ -34,22 +74,3 @@ function setSuccesMsg(input) {
     const formControl = input.parentElement;
     formControl.className = "form-control success";
 }
-
-
-
-
-//validate functions
-// const validate = () => {
-//     //values
-//     const usernameVal = username.value.trim();
-//     const phoneVal = phone.value.trim();
-//     const emailVal = email.value.trim();
-//     const passwordVal = password.value.trim();
-//     const cpasswordVal = cpassword.value.trim();
-//     //SUcess Message
-//     // const successMsg = () => {
-//     //     let frmCon = document.getElementsByClassName('form-control');
-//     //     for ();
-//     // }
-//     //username
-
